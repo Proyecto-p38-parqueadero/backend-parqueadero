365bad0dd9b69146256221aa14d7f152186d4bab
unknown <Carlos.cortes@Allianz.co>
2021-12-01 20:28:34 -0500

    despliegue heroku

--- parking/urls.py
+++ parking/urls.py
@@ -1,10 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include,re_path
-
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from parqueadero import views
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -24,7 +25,12 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
-    path("parqueadero/", include("parqueadero.urls")),
+    path("parking/create/", views.ParkingCreateView.as_view()),
+    #path("parking/list/", views.ParkingListView.as_view()),
+    path("parking/<int:pk>/", views.ParkingDetailView.as_view()), 
+    path("parking/update/<int:pk>/", views.ParkingUpdateView.as_view()), 
+    path("parking/delete/<int:pk>/", views.ParkingDeleteView.as_view()),
+    
     
     
 ]
